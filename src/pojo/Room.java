0250eca73b76bd28659b64f7b5ff564745e59c75
Oops <16302010059@fudan.edu.cn>
2018-10-23 17:29:41 +0800

    pojo对象

---
+++ src/pojo/Room.java
@@ -0,0 +1,64 @@
+package pojo;
+
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+
+
+@Component("room")
+public class Room {
+    private int kdno;
+    private int kcno;
+    private int ccno;
+    private String kdname;
+    private String papername;
+    private Timestamp exptime;
+
+    public int getKdno() {
+        return kdno;
+    }
+
+    public void setKdno(int kdno) {
+        this.kdno = kdno;
+    }
+
+    public int getKcno() {
+        return kcno;
+    }
+
+    public void setKcno(int kcno) {
+        this.kcno = kcno;
+    }
+
+    public int getCcno() {
+        return ccno;
+    }
+
+    public void setCcno(int ccno) {
+        this.ccno = ccno;
+    }
+
+    public String getKdname() {
+        return kdname;
+    }
+
+    public void setKdname(String kdname) {
+        this.kdname = kdname;
+    }
+
+    public String getPapername() {
+        return papername;
+    }
+
+    public void setPapername(String papername) {
+        this.papername = papername;
+    }
+
+    public Timestamp getExptime() {
+        return exptime;
+    }
+
+    public void setExptime(Timestamp exptime) {
+        this.exptime = exptime;
+    }
+}
---
+++ src/pojo/Student.java
@@ -0,0 +1,62 @@
+package pojo;
+
+
+import org.springframework.stereotype.Component;
+
+@Component("student")
+public class Student {
+    private int registno;
+    private int kdno;
+    private int kcno;
+    private int ccno;
+    private int seat;
+    private String name;
+
+    public int getRegistno() {
+        return registno;
+    }
+
+    public void setRegistno(int registno) {
+        this.registno = registno;
+    }
+
+    public int getKdno() {
+        return kdno;
+    }
+
+    public void setKdno(int kdno) {
+        this.kdno = kdno;
+    }
+
+    public int getKcno() {
+        return kcno;
+    }
+
+    public void setKcno(int kcno) {
+        this.kcno = kcno;
+    }
+
+    public int getCcno() {
+        return ccno;
+    }
+
+    public void setCcno(int ccno) {
+        this.ccno = ccno;
+    }
+
+    public int getSeat() {
+        return seat;
+    }
+
+    public void setSeat(int seat) {
+        this.seat = seat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
